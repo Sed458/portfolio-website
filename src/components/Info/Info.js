d4be0423a5197e542a2b8beab13371e3b85e0f12
Sed458 <sed458@nyu.edu>
2020-07-27 20:07:40 -0400

    Info and IconList stateless functional component

---
+++ src/components/Info/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Info.css';
+
+const Info = () => {
+    return (
+        <div className="info">
+            <h1>Sophia Danielsen</h1>
+            <h6>Simulation Engineer --- Creator --- Software Engineer</h6>
+        </div>
+    );
+}
+
+export default Info;
